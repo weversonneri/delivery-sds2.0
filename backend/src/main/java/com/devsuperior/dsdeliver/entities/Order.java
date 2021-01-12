11b3c535482a4a0e792b688359a7326343922351
Weverson <weversonneri@outlook.com>
2021-01-11 21:08:06 -0300

    Create endpoints

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -36,7 +36,7 @@ public class Order implements Serializable{
 	@JoinTable(name = "tb_order_product",
 			joinColumns = @JoinColumn(name = "order_id"),
 			inverseJoinColumns = @JoinColumn(name = "product_id"))
-	private Set<Product> Products = new HashSet<>();
+	private Set<Product> products = new HashSet<>();
 	
 	public Order () {
 		
@@ -101,7 +101,7 @@ public class Order implements Serializable{
 	}
 
 	public Set<Product> getProducts() {
-		return Products;
+		return products;
 	}
 
 	@Override
