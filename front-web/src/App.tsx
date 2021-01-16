fd6b0632c89a6258f0c46d0a41879472d819bee1
Weverson <weversonneri@outlook.com>
2021-01-16 01:43:24 -0300

    Add routes

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css';
 import './App.css';
-import Navbar from './Navbar';
-import Home from  './Home'
+import Routes from './Routes';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Home />
-    </div>
+
+      <Routes />
+   
   );
 }
 
